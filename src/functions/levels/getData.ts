6783dc46c88ad9e5f226a7da4bd0c205efff0893
Nasim Ahammed Nadim <90786729+3N147@users.noreply.github.com>
2022-07-13 21:29:22 +0600

    leaderboard type error fix

--- src/functions/levels/getData.ts
+++ src/functions/levels/getData.ts
@@ -7,26 +7,19 @@ export const getOrCreateLevelData: funcType = async (userId, guildId, getRank) =
 
     data = data ? data : await LevelDB.create({ userId, guildId })
 
-    if (getRank) {
-        const lb: LevelDataType[] = await LevelDB.find({ guildId })
-            .sort([["xp", "descending"]])
-            .exec()
+    if (getRank)
+        data.rank =
+            (await LevelDB.find({ guildId }).sort({ xp: -1 }).exec()).findIndex((i) => i.userId === userId) + 1 ?? 0
 
-        data.rank = lb.findIndex(i => i.userId === userId) + 1
-    }
     return data
 }
 
 export const getLevelData: funcType = async (userId, guildId, getRank) => {
     const data: LevelDataType = await LevelDB.findOne({ userId, guildId })
 
-    if (getRank && data) {
-        const lb: LevelDataType[] = await LevelDB.find({ guildId })
-            .sort([["xp", "descending"]])
-            .exec()
-
-        data.rank = lb.findIndex(i => i.userId === userId) + 1
-    }
+    if (getRank && data)
+        data.rank =
+            (await LevelDB.find({ guildId }).sort({ xp: -1 }).exec()).findIndex((i) => i.userId === userId) + 1 ?? 0
 
     return data as LevelDataType
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { config } from "dotenv"
+import { leaderboard } from "./functions/levels/leaderBoard"
 import { startServer } from "./server"
+
 config()
 
 import { ExtendedClient } from "./structures/Client"
@@ -9,3 +11,5 @@ export const client = new ExtendedClient()
 startServer("Server is online")
 
 client.start()
+
+leaderboard("957620528028483584", 100)
